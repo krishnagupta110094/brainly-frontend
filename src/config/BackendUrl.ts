44145405095e8f4ea124bda56248d8e6fe26a2a2
krishnagupta110094 <krishnagupta110094@gmail.com>
2026-01-08 16:15:14 +0530

    Backend_url

--- src/config/BackendUrl.ts
+++ src/config/BackendUrl.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000/api/v1";
+// export const BACKEND_URL = "http://localhost:3000/api/v1";
+export const BACKEND_URL = "https://brainly-backend-seven.vercel.app/api/v1";
